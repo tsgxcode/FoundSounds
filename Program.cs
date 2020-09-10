44c5f54c4f85f9a3d6464f23e586ad62e12cd1ad
TSG <TSG@TSGAC>
2020-09-10 07:23:10 -0700

    9.10.2020 Added more comments

--- Program.cs
+++ Program.cs
@@ -11,14 +11,17 @@ namespace FoundSounds
            A library of found sounds and the tools used to find them.
          */
 
+            //App Details
             string author = "T. Scott George (C) Sept - 2020";
             string appName = "Found Sound Library";
             string appVersion = "1.0";
 
+            //Making it pretty :)
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("{1} {0} {2}", author, appName, appVersion);
             Console.ResetColor();
 
+            //The  Serious Binnes, Jack
             Console.WriteLine("A world of found sounds");
         }
     }
