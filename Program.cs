b6ccd8682c4c5ef8720de14f51602f9749fbe691
TSG <TSG@TSGAC>
2020-09-10 06:59:52 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FoundSounds
+{
+    class Program
+    {
+        static void Main()
+
+        {
+            /* Found Sound - exploring the world of sound.
+           A library of found sounds and the tools used to find them.
+         */
+
+            string author = "T. Scott George (C) Sept - 2020";
+            string appName = "Found Sound Library";
+            string appVersion = "1.0";
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("{1} {0} {2}", author, appName, appVersion);
+            Console.ResetColor();
+
+            Console.WriteLine("A world of found sounds");
+        }
+    }
+}
